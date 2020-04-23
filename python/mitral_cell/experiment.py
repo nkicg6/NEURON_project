23c58b92c237b35473106924250df8d580d353e2
Nicholas George <nicholas.george32@gmail.com>
2020-04-22 21:43:16 -0600

    Black formatting.

--- python/mitral_cell/experiment.py
+++ python/mitral_cell/experiment.py
@@ -1,6 +1,7 @@
 # utilities to run experiments and save results
 import numpy as np
 
+
 def get_results(results_array, get_id):
-    arr = results_array['recording_vectors'][get_id]
+    arr = results_array["recording_vectors"][get_id]
     return np.asarray(arr).copy()
