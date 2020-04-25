b47a40e9b84f8ac204787ebcc60ac3e3ca2280c5
Nicholas George <nicholas.george32@gmail.com>
2020-04-25 14:15:57 -0600

    updated mitral cell model start adding realistic biophysics

--- python/mitral_cell/mitralcell.py
+++ python/mitral_cell/mitralcell.py
@@ -1,3 +1,7 @@
+# channel mod definitions from Hu et al. 2009 doi:10.1038/nn.2359
+# TODO! edit channel biophysics based on https://senselab.med.yale.edu/ModelDB/showmodel?model=263053&file=/zbili_debanne/myelinated_axon_1.hoc#tabs-2
+# TODO! figure out how to structure AIS biophysics
+
 import numpy as np
 from neuron import h
 from neuron.units import ms
@@ -5,6 +9,7 @@ from neuron.units import mV as mv
 
 
 h.load_file("stdrun.hoc")
+
 
 class MitralCell:
     def __init__(self, uid, nodes):
@@ -27,7 +32,7 @@ class MitralCell:
         h.celsius = self.experiment_temperature
         self._define_morphology()
         self._setup_morphology()
-        self._setup_biophysics()
+        self.setup_biophysics()
 
     def _make_ais(self):
         self.proximal_ais = h.Section(name="proximal_ais")
@@ -35,9 +40,7 @@ class MitralCell:
 
     def _make_myelinated_segments(self):
         for n in range(self.n_myelinated_segs):
-            self.myelinated_segs_list.append(
-                h.Section(name=f"myelin_seg_{n}")
-            )
+            self.myelinated_segs_list.append(h.Section(name=f"myelin_seg_{n}"))
 
     def _make_nodes(self):
         for n in range(self.n_nodes):
@@ -89,36 +92,58 @@ class MitralCell:
         self.proximal_ais.nseg = 9
         self.distal_ais.nseg = 3
 
-    def _setup_biophysics(self):
+    def setup_biophysics(self):
         """add all channels and passive properties"""
+        _uniform_passive_biophysics()
+        _proximal_ais_biophysics()
+        _distal_ais_biophysics()
+        _soma_biophysics()
+        _node_biophysics()
+        _dendrite_biophysics()
+        _myelinated_segments_biophysics()
+
+    def _uniform_passive_biophysics(self):
+        """adds universal passive properties/leak properties. can be updated individually per segment as well. Must be called first."""
         for section in self.all_segs:
             section.insert("pas")
-            section.Ra = 70
-            section.cm = 1.2
-            section.g_pas = 1/30000
-            section.e_pas = -65
+            section.Ra = 150
+            section.cm = 1.0
+            section.g_pas = 0.0000333
+            section.e_pas = -69.5
 
-        for section in self.active:
-            section.insert("na")
-            section.insert("kd")
-            section.ek = -90
-            section.ena = 60
+    def _soma_biophysics(self):
+        self.soma.insert("na12")
+        self.soma.insert("kv")
 
-        for section in self.passive:
-            for seg in section:
-                seg.pas.g = 0.001
-                seg.pas.e = -65
-
+    def _myelinated_segments_biophysics(self):
         for myelin_section in self.myelinated_segs_list:
             for myelin_seg in myelin_section:
                 myelin_seg.cm = 0.012
-                myelin_seg.g_pas = 1/100000
+                myelin_seg.g_pas = 1 / 100000
 
+    def _node_biophysics(self):
+        for section in self.nodes_list:
+            section.insert("na16")
+
+    def _dendrite_biophysics(self):
+        self.dend.insert("na12")
+        self.dend.insert("kv")
+
+    def _node_biophysics(self):
         for node_section in self.nodes_list:
             for node_seg in node_section:
-                node_seg.g_pas = 1/1000
-                node_seg.gbar_na = 10115
-                node_seg.gbar_kd = 100
+                node_seg.g_pas = 1 / 1000
+                node_seg.gbar_na16 = 10115
+
+    def _proximal_ais_biophysics(self):
+        self.proximal_ais.insert("na12")
+        self.proximal_ais.insert("na16")
+        self.proximal_ais.insert("kv")
+
+    def _distal_ais_biophysics(self):
+        self.distal_ais.insert("na16")
+        self.distal_ais.insert("kv")
+        self.distal_ais.insert("na12")
 
     def add_stim(self, stim_dict):
         self.stim_dict = stim_dict
