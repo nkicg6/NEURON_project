67ccfb51fc3e4494f5fc399421dfb3dd49a47727
Nicholas George <nicholas.george32@gmail.com>
2020-05-04 16:53:24 -0600

    start code for simulation AP analysis

---
+++ python/mitral_cell/actionpotential.py
@@ -0,0 +1,31 @@
+# action potential analysis
+import scipy.signal as s
+
+def ap_find_peak(y: np.array) -> tuple:
+    """use scipy to find the peak of the AP"""
+    p_inds, info = s.find_peaks(y, height = 10, prominence=None, distance=5)
+    if len(p_inds) == 0:
+        return (None, None)
+    return (p_inds, info)
+
+def ap_calculate_amplitude(y: np.array,ind: np.array, rmp: float) -> float:
+    """return the amplitude of the action potential"""
+    if isinstance(ind, type(None)):
+        raise AssertionError("Index not found")
+    assert len(ind) == 1, f"Length of indicies must be = 1, length = {len(ind)}"
+    assert rmp < 0, f"resting membrane potential must be negative, rmp={rmp}"
+    amp_peak = y[ind][0]
+    return float(abs(rmp) + abs(amp_peak))
+
+def ap_batch_amplitude(list_of_traces: list, rmp: float) -> list:
+    """composed function to map over a list of numpy arrays representing a series of NEURON simulations to calculate amplitude
+    """
+    amps = []
+    for experiment in list_of_traces:
+        inds, _ = ap_find_peak(experiment)
+        try:
+            amp = ap_calculate_amplitude(experiment, inds, rmp)
+            amps.append(amp)
+        except AssertionError:
+            amps.append(0)
+    return amps
