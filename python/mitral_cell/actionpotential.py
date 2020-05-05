a5329b922a655f577e4d9fa661f4da1fa7587797
Nicholas George <nicholas.george32@gmail.com>
2020-05-04 22:47:35 -0600

    updated ap_calculate_amplitude test. passing

--- python/mitral_cell/actionpotential.py
+++ python/mitral_cell/actionpotential.py
@@ -3,20 +3,18 @@ import numpy as np
 import scipy.signal as s
 
 def ap_find_peak(y: np.ndarray) -> int:
-    """use scipy to find the peak of the AP"""
+    """use scipy to find the peak of the AP. Fails if multiple peaks or no peaks"""
     p_inds, _ = s.find_peaks(y, height = 10, prominence=None, distance=5)
     if len(p_inds) != 1:
         raise AssertionError(f"Must be one peak. {len(p_inds)} detected")
     int_ind = p_inds[0]
     return int(int_ind)
 
-def ap_calculate_amplitude(y: np.array,ind: np.array, rmp: float) -> float:
+def ap_calculate_amplitude(y: np.ndarray, ind: int, rmp: float) -> float:
     """return the amplitude of the action potential"""
-    if isinstance(ind, type(None)):
-        raise AssertionError("Index not found")
-    assert len(ind) == 1, f"Length of indicies must be = 1, length = {len(ind)}"
-    assert rmp < 0, f"resting membrane potential must be negative, rmp={rmp}"
-    amp_peak = y[ind][0]
+    assert len(y) > ind, "Index out of range. length array = {len(y)}, index = {ind}"
+    assert rmp<0, "RMP should be negative, did you make a mistake? RMP = {rmp}"
+    amp_peak = y[ind]
     return float(abs(rmp) + abs(amp_peak))
 
 def ap_batch_amplitude(list_of_traces: list, rmp: float) -> list:
--- python/tests/test_actionpotential.py
+++ python/tests/test_actionpotential.py
@@ -16,3 +16,20 @@ def test_ap_find_peak():
     two_peak[20] = 15
     with pytest.raises(AssertionError):
         ap.ap_find_peak(two_peak)
+    no_peaks = np.zeros(20)
+    with pytest.raises(AssertionError):
+        ap.ap_find_peak(no_peaks)
+
+
+def test_ap_calculate_amplitude():
+    arr = np.zeros(20)
+    arr[10] = 15
+    ind = ap.ap_find_peak(arr)
+    assert ap.ap_calculate_amplitude(arr, ind, -65) == 80
+    # index out of range
+    with pytest.raises(AssertionError):
+        ap.ap_calculate_amplitude(arr, 30, -65)
+    with pytest.raises(AssertionError):
+        ap.ap_calculate_amplitude([], 30, -65)
+    with pytest.raises(AssertionError):
+        ap.ap_calculate_amplitude(arr, 30, 65)
